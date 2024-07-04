c19cc5ea007167c0d6eafe4e98e6f2ab3a9a85f7
jsohnData <98851246+jsohndata@users.noreply.github.com>
2024-07-04 05:33:50 +0000

    UPDATE style

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ canvas.height = window.innerHeight;
 // Configurable settings object
 const config = {
     colors: ['rgba(255, 87, 51, 0.5)', 'rgba(51, 255, 87, 0.5)', 'rgba(51, 87, 255, 0.5)', 'rgba(243, 51, 255, 0.5)', 'rgba(51, 255, 245, 0.5)'],
-    minRadius: 10,
+    minRadius: 20,
     maxRadius: 100,
     pulseFactor: 0.001,  // Much slower pulse rate
     fadeSpeed: 0.001,    // Much slower fade speed
