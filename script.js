c2088e51f738194f60c67f0ca69b309caa1dd44a
jsohnData <98851246+jsohndata@users.noreply.github.com>
2024-07-04 06:13:26 +0000

    UPDATE script

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ canvas.height = window.innerHeight;
 const config = {
     colors: ['rgba(255, 87, 51, 0.5)', 'rgba(51, 255, 87, 0.5)', 'rgba(51, 87, 255, 0.5)', 'rgba(243, 51, 255, 0.5)', 'rgba(51, 255, 245, 0.5)'],
     minRadius: 20,
-    maxRadius: 100,
-    pulseFactor: 0.001,  // Much slower pulse rate
-    fadeSpeed: 0.001,    // Much slower fade speed
-    bounceFactor: 0.08      // Gentle bounce effect
+    maxRadius: 400,
+    pulseFactor: 0.1,  // Much slower pulse rate
+    fadeSpeed: 0.005,    // Much slower fade speed
+    bounceFactor: 0.8      // Gentle bounce effect
 };
 
 class Circle {
